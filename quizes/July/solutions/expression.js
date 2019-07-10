f302d40ca7cf75a8766b83a52a3c72c07f18485c
Alaa Mohammad <alaa.mohammadhb@gmail.com>
2019-07-10 11:20:52 +0300

    delete

--- quizes/July/solutions/expression.js
+++
@@ -1,9 +0,0 @@
-/*
- * Programming Quiz: First Expression
- *
- * Write an expression that uses at least three, different, arithmetic operators
- * to log the number 42 to the console.
- */
-
-// this expression equals 4, change it to equal 42
-console.log(20 * 2  + 4 -2);
--- quizes/July/solutions/favorite-food.js
+++
@@ -1,8 +0,0 @@
-/*
- * Programming Quiz: Favorite Food 
- * Create a string with the name of your favorite food. The first letter of the string should be capitalized.
- */
- 
- var myFav = "Orange";
-
-console.log(myFav);
