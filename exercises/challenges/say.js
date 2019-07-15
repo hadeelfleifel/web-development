43bf245c135439da1eaa656c64322f2345bc28cc
Alaa Mohammad <alaa.mohammadhb@gmail.com>
2019-07-15 12:33:55 +0300

    challenges

---
+++ exercises/challenges/say.js
@@ -0,0 +1,14 @@
+/*Given a number from 0 to 999,999,999,999, spell out that number in English.
+* Instructions:
+* Handle the basic case of 0 through 99.
+* If the input to the program is 22, then the output should be 'twenty-two'.
+* Your program should complain loudly if given a number outside the blessed range.
+* Some good test cases for this program are:
+0
+14
+50
+98
+-1
+100
+*/
+
