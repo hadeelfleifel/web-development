de64c670816f0824e7dad8f0428a19ec3d2e9b1b
Alaa Mohammad <alaa.mohammadhb@gmail.com>
2019-07-18 16:56:14 +0300

    extra

---
+++ exercises/js/Reduce-Filter/extra-exe.js
@@ -0,0 +1,67 @@
+/*
+12
+Using Filter
+Create a function called longerThan
+that accept an array of strings
+and return the longer string than the number
+var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
+Ex: longerThan(strings,4) => ["mercer","madrasa","rashed2"]
+*/
+
+/*
+13
+Using Filter
+Create a function called onlyOneWord
+that accept an array of strings
+and return only those strings with a single word (no spaces)
+var strings= [ 'return', 'phrases', 'with one word' ];
+Ex: onlyOneWord(strings) => [ 'return', 'phrases' ]
+*/
+
+
+
+/*
+14
+Using Filter
+Create a function called positiveRowsOnly
+that accept an array of array of numbers(matrix)
+and return only the rows in the matrix that have all positive integers
+var numbers= [[ 1, 10, -100 ], [ 2, -20, 200 ],[ 3, 30,  300 ]];
+Ex: positiveRowsOnly(numbers) => [ 3, 30,  300 ]
+*/
+
+
+/*
+15
+Using Filter
+Create a function called allSameVowels
+that accept an array of strings
+return only those words where all the vowels are the same
+var strings= [ 'racecar', 'amalgam', 'oligopoly', 'zoom' ];
+Ex: allSameVowels(strings) =>  [ 'amalgam', 'zoom' ]
+*/
+
+//Using Reduce 16 -
+
+/*
+16
+Using Reduce
+Create a function called objectify
+that accept an array of pairs array
+and turns an array of arrays into an object
+var array= [[ 'Thundercats', '80s' ],[ 'The Powerpuff Girls', '90s' ],[ 'Sealab 2021', '00s' ]]
+Ex: objectify(array) =>  { 'Thundercats': '80s', 'The Powerpuff Girls': '90s', 'Sealab 2021': '00s' };
+*/
+
+
+/*
+17
+Using Reduce
+Create a function called luckyNumbers
+that accept an array of number
+and turns an array of arrays into an object
+var array= [ 30, 48, 11, 5, 32 ]
+Ex: luckyNumbers(array) => 'Your lucky numbers are: 30, 48, 11, 5, and 32';
+*/
+
+// if you finish the exercises review the material of the week and help your classmate
